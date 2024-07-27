cb916596975733dce671085d5446913d68ebacc4
Paulo Sergio Avila Junior <pauloffxii@hotmail.com>
2024-07-27 18:00:59 -0300

    Testes iniciais com typescript

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({extended: true}));
+
+app.post('/submit', (req,res) => {
+    Logger.log({
+        name: req.body.name,
+        message: req.body.message,
+    });
+    res.send('Thanks for your message :)');
+})
+
+app.get('/teste', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/teste.html'));
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
