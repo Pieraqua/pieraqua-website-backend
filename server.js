254696c01e07d339046f6408c479e1d2a3ec8c28
pieraqua <pauloffxii@hotmail.com>
2024-08-05 16:26:26 -0300

    docker support

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.urlencoded({extended: true}));
 
 app.use((req, res, next) => { 
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080"); 
   res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization" ); 
   next(); 
 });
